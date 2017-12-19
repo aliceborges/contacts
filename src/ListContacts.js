046351fe1047cfa93b89bad269522637d317ec92
Alice Borges <aliceborges@outlook.com.br>
2017-12-18 22:53:29 -0200

    Desestruturação de objetos

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -20,13 +20,16 @@ class ListContacts extends Component{
 
 	render(){
 
+		const { contacts, onDeleteContact }  = this.props
+		const { query } = this.state
+
 		let showingContacts 
-		if(this.state.query){
-			const match = new RegExp(escapeRegExp(this.state.query), 'i')
-			showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+		if(query){
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingContacts = contacts.filter((contact) => match.test(contact.name))
 		}
 		else{
-			showingContacts = this.props.contacts
+			showingContacts = contacts
 		}
 
 		showingContacts.sort(sortBy('name'))
@@ -52,7 +55,7 @@ class ListContacts extends Component{
 							<p>{contact.name}</p>
 							<p>{contact.email}</p>
 						</div>
-						<button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>Remove</button>
+						<button onClick={() => onDeleteContact(contact)} className='contact-remove'>Remove</button>
 						</li>
 						))}
 				</ol>
