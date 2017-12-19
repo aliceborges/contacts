81982d98cd8215612616db573f202fcb4de3843d
Alice Borges <aliceborges@outlook.com.br>
2017-12-18 22:31:45 -0200

    Validando os campos com PropTypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function ListContacts (props) {
 	return (
@@ -16,7 +17,12 @@ function ListContacts (props) {
 				</li>
 				))}
 		</ol>
-		)
+	)
+}
+
+ListContacts.propTypes = {
+	contacts: PropTypes.array.isRequired,
+	onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts
